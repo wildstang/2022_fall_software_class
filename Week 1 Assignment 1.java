41f3f2dc4b037ab1ba6ab93c629253f32b746bb7
Stuart McCrorie <42500981+smccrorie@users.noreply.github.com>
2022-08-28 21:19:13 -0500

    Update Week 1 Assignment 1.java

--- Week 1 Assignment 1.java
+++ Week 1 Assignment 1.java
@@ -3,10 +3,16 @@
 ///////////////////////////////////////
 //Add yourself to the roster file in the main directory
 
-//First, create your own branch, titled "MYFIRSTNAME_branch". Do this by viewing the code tab, finding "branches" right under the tab bar, clicking that, and finding the "new branch" button.
+//First, create your own branch, titled "MYFIRSTNAME_branch". Do this by viewing the code tab, 
+//finding "branches" right under the tab bar, clicking that, and finding the "new branch" button.
 
-//Then, on your own branch, click on the roster file, then the pencil icon to edit it. Add in a new row to the table that contains your full name, GitHub username, the team number you are participating on this season (111/112), and the year you are expected to graduate high school. 
+//Then, on your own branch, click on the roster file, then the pencil icon to edit it. Add in a 
+//new row to the table that contains your full name, GitHub username, the team number you are 
+//participating on this season (111/112), and the year you are expected to graduate high school. 
 
 //Commit your changes by filling out the box at the bottom of the page once you've added your own data.
 
-//Once this is done, create a pull request on GitHub by finding the Pull Requests tab and clicking on Create Pull Request. Choose your branch from the "compare: main" dropdown. Click to create the pull request, then add fruzyna and smccrorie as reviewers. Finally, click create pull request to create this request, and be done with this assignment.
+//Once this is done, create a pull request on GitHub by finding the Pull Requests tab and clicking 
+//on Create Pull Request. Choose your branch from the "compare: main" dropdown. Click to create the 
+//pull request, then add fruzyna and smccrorie as reviewers. Finally, click create pull request to 
+//create this request, and be done with this assignment.
