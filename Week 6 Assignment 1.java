27ed083ade0f3e51af2f1119129b902756294347
Liam Fruzyna <liamfruzyna@gmail.com>
2022-09-04 09:53:06 -0500

    Minor tweaks and corrections plus readability improvements

--- Week 6 Assignment 1.java
+++ Week 6 Assignment 1.java
@@ -1,40 +1,49 @@
-//WildStang Fall Software Week 6 Assignment 1
+// WildStang Fall Software Week 6 Assignment 1
 
-//As always, edit this file while in your branch of this folder (repository)
+// As always, edit this file while in your branch of this folder (repository)
 
 ///////////////////////////////////////
-//Take the subsystem you coded for last week, and add in a couple of methods and autonomous steps to enable it to be used in autonomous. 
-//You should add some methods to the subsystem so that an autonomous step can control the subsystem.
+// Take the subsystem you coded for last week, and add in a couple of methods and autonomous steps to enable it to be used in autonomous. 
+// You should add some methods to the subsystem so that an autonomous step can control the subsystem.
 
-//Steps to add
-//Deploy intake (and run intake wheels)
-//retract intake (and turn off intake wheels)
-//turn on/off feed (should take a boolean argument - true for on, false for off)
-//set shooter speed (with a double argument for the power)
-//turn shooter on/off
+// Steps to add
+// Deploy intake (and run intake wheels)
+// retract intake (and turn off intake wheels)
+// turn on/off feed (should take a boolean argument - true for on, false for off)
+// set shooter speed (with a double argument for the power)
+// turn shooter on/off
 
-public void deployIntake(){
-//your code goes here
-
-
+public void deployIntake() {
+	//your code goes here
+	
+	
+	
 }
-public void retractIntake(){
-//your code goes here
 
-
+public void retractIntake() {
+	//your code goes here
+	
+	
+	
 }
-public void feedOnOff(boolean isOn){
-//your code goes here
 
-
+public void feedOnOff(boolean isOn) {
+	//your code goes here
+	
+	
+	
 }
-public void setShooterSpeed(double speed){
-//your code goes here
 
-
+public void setShooterSpeed(double speed) {
+	//your code goes here
+	
+	
+	
 }
-public void shooterOnOff(boolean isOn){
-//your code goes here
 
-
+public void shooterOnOff(boolean isOn) {
+	//your code goes here
+	
+	
+	
 }
