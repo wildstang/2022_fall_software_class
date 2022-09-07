d0dc5496ef98d76e92da4eeb89ce1ef4ad74a660
Ewetzel5400 <90660042+Ewetzel5400@users.noreply.github.com>
2022-09-06 20:04:34 -0500

    Update Week 1 Assignment 2.java

--- Week 1 Assignment 2.java
+++ Week 1 Assignment 2.java
@@ -9,17 +9,17 @@
 // Section 1 - Interger Math
 
 int a = 2 + 5;
-a == ___;
+a == _7__;
 int b = 45 - 90;
-b == ___;
+b == __-45_;
 int c = 2 * 7;
-c == ___;
+c == _14__;
 int d = 34 % 5;
-d == ___;
+d == _5__;
 int e = 3 * 0;
-e == ___;
+e == __0_;
 int f = 4 * ((3 + 7) / 2);
-f == ___;
+f == __20  _;
 
 
 // Section 2 - Assignment Math
@@ -28,54 +28,54 @@ int a = 6;
 int b = 4;
 a = a + b;
 b = a - b;
-a == ___;
-b == ___;
+a == __10_;
+b == __6_;
 int c = 2;
 int d = 3;
 c = 4 + d;
 d = d * 3;
 c = 4 + d;
-c == ___;
-d == ___;
+c == __13_;
+d == __9_;
 int e = 12;
 int f = 3;
 e = e / f + 1;
 f = 3 + f * 2;
-e == ___;
-f == ___;
+e == _5__;
+f == _9__;
 int g = 10;
 int h = 2;
 g = g - h / 2 * 6;
 h = (3 + 1) * (4 * 2 - 1) - 4 / 2 + 2;
-g == ___;
+g == _9.5__;
 h == ___;
 int g = 5;
 int h = g++;
-g == ___;
-h == ___;
+g == __5_;
+h == 6___;
 int i = 5;
 int j = ++i;
-i == ___;
-j =k ___;
+i == _5__;
+j =k 5___;
 int k = 5;
 int l = (k++) + 3;
 int m = 2 * k + (++l);
 l = 2 * (--m) - (k++);
-k == ___;
-l == ___;
-m == ___;
+k == __5_;
+l == 2___;
+m == 3___;
 
 // Section 3 - Boolean Logic
 
 boolean x = 5>4;
-x == ___;
+x == _5__;
 boolean y = 3 < 4 && 5 >= 6;
-y == ___;
+y == __3_;
 boolean z = 5 == 10 / 2 || 7 == 7 * 2;
-z == ___;
+z == __14_;
 boolean u = !(true || false) && !(!true || !false);
-u == ___;
+u == __true_;
 boolean m = ((true && (!false && true)) || !(false && true)) && true;
-m == ___;
+m == __true_;
 boolean n = (true && (!true ^ true)) ^ !(false || (!true));
-n == ___;
+n == _false__;
