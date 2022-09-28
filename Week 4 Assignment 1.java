69599123747ee725ae5fd74877f348119987dd4b
Michael Bauer <90660076+MichaelBauer6@users.noreply.github.com>
2022-09-27 20:15:38 -0500

    Update Week 4 Assignment 1.java

--- Week 4 Assignment 1.java
+++ Week 4 Assignment 1.java
@@ -16,7 +16,51 @@ delayStep(); // does nothing for some seconds
 // at (1,7), (3,3), (7,6), and (9,1). There is a goal at (10,10) to score the cargo in. We want
 // to score all 4 cargo, using the 5 steps from above
 
+Rover robot = new Rover();
+Rover.moveto(1, 7);
+Rover.moveto(7, 6);
 
+
+class Rover {
+  private xlocation = 0;
+  private ylocation = 0
+  private heading = 0
+  void moveto(int x, int y) {
+    // Turning
+    
+    int xside = Math.abs(xlocation - x);
+    int yside = Math.abs(ylocation - y);
+    
+    double angle = Math.atan((double) yside / xside);
+      
+    int destheading;
+    
+    // First quadrant
+    if (x > xlocation && y > ylocation) {
+      destheading = 0 + angle;
+    }
+    
+    // Second quadrant
+    else if (x > xlocation && y < ylocation) {
+      destheading = 180 - angle;
+    }
+    
+    // Third quadrant
+    else if (x < xlocation && y < ylocation) {
+      destheading - 180 + angle;
+    }
+    
+    // Fourth quadrant
+    else if (x < xlocation && y > ylocation) {
+      destheading = 360 - angle; 
+    }
+    
+    turnstep(destheading - heading);
+    
+    // Move
+    driveStep(Math.sqrt(Math.pow(xside, 2) + Math.pow(yside, 2)));
+  }
+}
 
 
 
