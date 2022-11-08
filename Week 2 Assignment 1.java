bfd2290b07054efc634c0bb1d35c89a748649fc3
Keaton-Meier <116606640+Keaton-Meier@users.noreply.github.com>
2022-11-07 20:16:06 -0600

    Update Week 2 Assignment 1.java

--- Week 2 Assignment 1.java
+++ Week 2 Assignment 1.java
@@ -6,10 +6,10 @@
 // Activity 1 - assign the variable "answer" to the factorial of the input variable "input"
 
 int input = 8; // this could be any whole positive interger number
-int answer;
+int answer = 40320;
 //your code goes below
-
-
+for (int i=input; i>0; i--)
+{answer * = i;}
 
 
 
@@ -20,9 +20,12 @@ int answer;
 
 int total = 36; // this could be any whole positive interger number
 int divisor = 13; // this could be any whole positive interger number
-int answer;
+int answer = 10;
 // your code goes below
-
+answer=total;
+while answer > 0;
+{answer-=divisori}
+answer+=divisori
 
 
 
@@ -34,10 +37,14 @@ int answer;
 // so if the input "nth" is 3, the answer is 2. If "nth" is 4, the answer is 3. If "nth" is 5, the answer is 5. If "nth" is 6, answer is 8 etc.
 
 int nth = 21; // this could be any whole positive interger number
-int answer;
+int answer = 108167;
 // your code goes below
-
-
+int a= 0;
+int b= 1;
+for (int i=0; i <nth; i++
+     {answer = a+b
+       a=b;
+      b= answer;}
 
 
 
@@ -49,6 +56,12 @@ int answer;
 
 int length = 50; // this could be any whole positive interger number
 // your code goes below
+     int lowest=0;
+for (int i=0; i< array.length; i++)
+     if (array[i]<array[lowest])
+     {lowest=i;}
+     for(int j=0; j,array.length; j++)
+     {answer[j]=lowest;}
 
 
 
